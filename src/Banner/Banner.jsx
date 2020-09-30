252f497aa4b9bdff09d9c4da417e156ec1a9456b
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-30 21:26:33 +0545

    -banner buttons added and styles fixed

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
 import requests from "../requests";
-
+import "../styles/Banner.css";
 function Banner(props) {
   const [movie, setMoive] = useState([]);
   useEffect(() => {
@@ -16,23 +16,32 @@ function Banner(props) {
     fetchData();
   }, []);
   console.log(movie);
+
+  function truncate(str,n){
+    return str?.length>n?str.substr(0,n-1)+"...":str;
+  }
+
   return (
-    <header className="banner"
-    style={{
-      backgroundSize:"cover",
-      backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-      backgroundPosition:"center center",
-    }}>
-        {/*background image*/}
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      {/*background image*/}
       <div className="banner__contents">
         {/*title*/}
-        <h1>
-          {movie?.title ||movie?.name||movie?.original_name}
-        </h1>
-        <div className="banner__button">
-
+        <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
         </div>
         {/*DIV->2 OTA BUTTON*/}
+        <h1 className="banner__description">
+          {truncate(movie?.overview,150)}
+        </h1>
         {/*description*/}
       </div>
     </header>

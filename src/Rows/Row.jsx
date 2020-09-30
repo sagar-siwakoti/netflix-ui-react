24e8bb2922b518ff3c075ee5e0fad73490458e34
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-30 17:35:02 +0545

    -added scroll transition and fix api bug

--- src/Rows/Row.jsx
+++ src/Rows/Row.jsx
@@ -1,11 +1,35 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../axios";
+import "../styles/Row.css"
 
-function Row({ title }) {
-    const [movies,setMovies]=useState([]);
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+  //A snippet of code which runs based on a specific condition/variable
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.table(movies);
+
   return (
-    <div>
+    <div className="row">
       {/*title*/}
       <h2>{title}</h2>
+      <div className="row__posters">
+        {/*posters*/}
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
       {/*container with posters*/}
       {/*posters*/}
     </div>

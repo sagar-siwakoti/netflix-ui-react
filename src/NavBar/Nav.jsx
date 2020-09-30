5a2e7f8562b034b54601fe6d6f64d60743024ae1
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-30 22:05:19 +0545

    -nav bar added with styles

--- src/NavBar/Nav.jsx
+++ src/NavBar/Nav.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
-
+import React from "react";
+import "../styles/Nav.css";
 function Nav(props) {
-    return (
-        <div></div>
-    );
+  return (
+    <div className="nav">
+      <img
+        className="nav__logo"
+        src="https://www.freepnglogos.com/uploads/red-netflix-logo-text-png-3.png"
+        alt="Netflix logo"
+      />
+        <img
+            className="nav__avatar"
+            src="https://pbs.twimg.com/profile_images/124011999041155"
+            alt="Netflix avatar"
+        />
+    </div>
+  );
 }
 
 export default Nav;

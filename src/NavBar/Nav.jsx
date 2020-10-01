6955006f77e1b53f18d28596ab0ef52b2e593213
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-01 13:01:48 +0545

    -nav bar styles finised

--- src/NavBar/Nav.jsx
+++ src/NavBar/Nav.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../styles/Nav.css";
+
 function Nav(props) {
+    const [show,handleShow]=useState(false);
+useEffect(()=>{
+    window.addEventListener("scroll",()=>{
+        if (window.scrollY>100){
+            handleShow(true);
+        }else handleShow(false);
+    });
+    return ()=>{
+        window.removeEventListener("scroll");
+    }
+},[]);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img
         className="nav__logo"
         src="https://www.freepnglogos.com/uploads/red-netflix-logo-text-png-3.png"

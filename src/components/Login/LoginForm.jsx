f6c9ca61ae640944e9d8eb9ab7ba71797e417579
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-10-02 19:21:12 +0545

    -login styles added

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-
+import "./LoginForm.css";
 function LoginForm(props) {
   return (
-
+    <div className="form__containerOutside">
       <div className="form__container">
         <form>
           <h1>Sign In</h1>
           <div className="input__container">
-            <input type="email" required />
+            <input className="input__empty" type="email" required />
             <label>Email or Phone Number</label>
           </div>
           <div className="input__container">
-            <input type="password" required />
+            <input className="input__empty" type="password" required />
             <label>Password</label>
           </div>
-            <div className="input__container">
-                <button type="submit">Sign In</button>
-            </div>
+          <div className="input__container">
+            <button type="submit">Sign In</button>
+          </div>
         </form>
       </div>
-
+    </div>
   );
 }
 
